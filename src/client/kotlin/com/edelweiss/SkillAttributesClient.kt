e2b6c428823e40254ec870c1eef830cf89df0de6
Edelweiss <redwitblue@gmail.com>
2024-03-30 14:50:24 -0500

    Initialize mod for PR

---
+++ src/client/kotlin/com/edelweiss/SkillAttributesClient.kt
@@ -0,0 +1,9 @@
+package com.edelweiss
+
+import net.fabricmc.api.ClientModInitializer
+
+object SkillAttributesClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
